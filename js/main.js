a0a0c5247c22e1dccde0d854a4d9a61da50a301d
Thalita M <thalitamh5@gmail.com>
2022-05-09 11:42:02 -0300

    js pronto

--- js/main.js
+++ js/main.js
@@ -6,12 +6,21 @@ const Main = { // Main letra maiscula é por ser o objeto principal
     },
     cacheSelector: function () { //add elementos do html e armazenar numa variavel
         this.$checkButtons = document.querySelectorAll('.check')
+        this.$inputTask = document.querySelector('#inputTask')
+        this.$list = document.querySelector('#list')
+        this.$removeButtons = document.querySelectorAll('.remove')
     },
 
     bindEvents: function () { //resposavel add eventos (de click,+ e teclas)
         const self = this
         this.$checkButtons.forEach(function (button) {
             button.onclick = self.Events.checkButton_click
+        })
+        this.$inputTask.onkeypress = self.Events.inputTask_keypress.bind(this)
+
+        this.$removeButtons.forEach(function(button){
+            button.onclick = self.Events.removeButton_click
+
         })
     },
 
@@ -24,9 +33,42 @@ const Main = { // Main letra maiscula é por ser o objeto principal
             if (!isDone) {
                 return li.classList.add('done')
             }
-            li.classList.remove('done')
-        }
 
+            li.classList.remove('done')
+        },
+
+        inputTask_keypress: function (e) {
+            const key = e.key
+            const value = e.target.value
+
+            if (key === 'Enter') {
+                this.$list.innerHTML += `
+
+                    <li>
+                        <div class="check"></div>
+                        <label class="task">
+                            ${value}
+                        </label>
+                        <button class="remove"></button>
+                    </li>
+                `
+                e.target.value = ''
+                
+                this.cacheSelector()
+                this.bindEvents()
+            }
+        },
+
+       removeButton_click: function(e){
+           let li = e.target.parentElement
+
+           li.classList.add('removed')
+
+           setTimeout(function(){
+                li.classList.add('hidden')
+           },300)
+
+       }
     }
 }
 Main.init()
